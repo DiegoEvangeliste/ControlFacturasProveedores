8301a3e7b16d384b297dd561314b35c4b749c54e
DiegoEvangeliste <evangelistediego@yahoo.com.ar>
2024-01-04 09:16:07 -0300

    structure was added of packages and classes

---
+++ src/main/java/com/DiegoEvangeliste/ControlFacturasProveedores/model/entity/Factura.java
@@ -0,0 +1,65 @@
+package com.DiegoEvangeliste.ControlFacturasProveedores.model.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "facturas")
+public class Factura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "estadoPago")
+    private Boolean estadoPago;
+
+    @Column(name = "puntoVenta")
+    private Integer puntoVenta;
+
+    @Column(name = "numeroComprobante")
+    private Integer numeroComprobante;
+
+    @Column(name = "numeroRemito")
+    private Integer numeroRemito;
+
+    @Column(name = "fechaEmision")
+    private LocalDate fechaEmision;
+
+        // Relacion IDA con Proveedor
+//    @ManyToOne
+//    @JoinColumn(name="id_proveedor")
+//    private Proveedor proveedor;
+
+        // Relacion IDA con Producto
+//    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+//    @JoinTable(name="tbl_facturas_productos", joinColumns = @JoinColumn(name="id_factura"), inverseJoinColumns = @JoinColumn(name="id_producto"),  uniqueConstraints = @UniqueConstraint(columnNames = {"id_factura", "id_producto"}))
+//    private List<Producto> productos;
+
+}
---
+++ src/main/java/com/DiegoEvangeliste/ControlFacturasProveedores/model/entity/Producto.java
@@ -0,0 +1,43 @@
+package com.DiegoEvangeliste.ControlFacturasProveedores.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precio")
+    private Double precio;
+
+        // Relacion VUELTA con Factura
+//    @ManyToMany(mappedBy = "productos")
+//    private List<Factura> facturas;
+
+}
